61baf2d5c3e987a91b4c6b630cbc3790b45ed9e4
Matt Nodurfth <mnodurft@gmail.com>
2013-10-24 11:24:25 +0800

    Rails form way in edit/new html.slims, added better error gems, updated routes to override stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,19 @@
 Project2::Application.routes.draw do
 
-	
 	#Sign in/out
-	get    'login' => 'session#new'
+	get    'login' => 'session#new', as: :login
 	post   'login' => 'session#create'
-	delete 'logout' => 'session#destroy'  #delete goes to destry (just remember it)
+	delete 'logout' => 'session#destroy',  as: :logout #delete goes to destry (just remember it)
 	get    'logout' => 'session#destroy'
 
 	# Registration
-	get  'register' => 'registration#new'
+	get  'register' => 'registration#new', as: :register
 	post 'register' => 'registration#create'
 
 	# Password reset
-	get 'reset/:code'=> 'password#edit'
+	get 'reset/:code'=> 'password#edit', as: :reset
 	put 'reset/:code'=> 'password#update'
 	patch 'reset/:code'=> 'password#update' # only updates the part altered
-
-
-
 
 	get 'privacy' => 'site#privacy'
 	get 'terms' => 'site#terms'  # routes to the pages, calling the controller and the action on that controller
@@ -26,3 +22,7 @@ Project2::Application.routes.draw do
   root 'site#index'
 
 end
+
+
+
+# but we can't have people frustrated and not understand the issues
